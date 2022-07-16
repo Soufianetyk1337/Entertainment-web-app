63a71d9b5b3b4c2219cd67b511dbfb78f742f8fe
Soufianetyk1337 <sz.fanel@gmail.com>
2022-07-16 20:06:27 +0100

    Series View with styles

---
+++ src/components/Series/Series.jsx
@@ -0,0 +1,25 @@
+import { VideosContext } from "@/contexts/VideosContext";
+import React, { useContext } from "react";
+import Searchbar from "../SearchBar/Searchbar";
+import VideoList from "../Video/VideoList/VideoList";
+import "./Series.scss";
+const Series = () => {
+  const { getters, search } = useContext(VideosContext);
+  const { series, filteredSeries } = getters;
+  return (
+    <>
+      <Searchbar placeholder="Search for TV series" />
+      {!search ? (
+        <VideoList className="tv-series" title="TV Series" videos={series()} />
+      ) : (
+        <VideoList
+          className="results"
+          title="`Found ${filteredSeries.length} results for '${search}'`"
+          videos={filteredSeries()}
+        />
+      )}
+    </>
+  );
+};
+
+export default Series;
