f91447c7caeb145af8f0963d716a9405d94a41b2
Soufianetyk1337 <sz.fanel@gmail.com>
2022-07-16 06:40:09 +0100

    Navigation Bar Icons

---
+++ src/components/icons/EmptyBookmark.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const EmptyBookmark = () => {
+  return (
+    <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        fill="transparent"
+      />
+    </svg>
+  );
+};
+
+export default EmptyBookmark;
