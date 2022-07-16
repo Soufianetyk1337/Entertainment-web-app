74c2e14a94b666073226187b7b08b07cf88e1bc0
Soufianetyk1337 <sz.fanel@gmail.com>
2022-07-16 20:07:09 +0100

    Movies View with styles

---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,25 @@
+import { VideosContext } from "@/contexts/VideosContext";
+import React, { useContext } from "react";
+import Searchbar from "../SearchBar/Searchbar";
+import VideoList from "../Video/VideoList/VideoList";
+import "./Movies.scss";
+const Movies = () => {
+  const { getters, search } = useContext(VideosContext);
+  const { movies, filteredMovies } = getters;
+  return (
+    <>
+      <Searchbar placeholder="Search for movies" />
+      {!search ? (
+        <VideoList className="movies" title="Movies" videos={movies()} />
+      ) : (
+        <VideoList
+          className="results"
+          title="`Found ${filteredMovies.length} results for '${search}'`"
+          videos={filteredMovies()}
+        />
+      )}
+    </>
+  );
+};
+
+export default Movies;
