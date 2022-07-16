5124150cc9bd0ccd0113c93f9780a4a6ab420ae3
Soufianetyk1337 <sz.fanel@gmail.com>
2022-07-16 20:05:50 +0100

    Bookmark View

---
+++ src/components/Bookmark/Bookmark.jsx
@@ -0,0 +1,43 @@
+import React, { useContext } from "react";
+import Searchbar from "@/components/SearchBar/Searchbar";
+import VideoList from "@/components/Video/VideoList/VideoList";
+import { VideosContext } from "@/contexts/VideosContext";
+import "./Bookmark.scss";
+const Bookmark = () => {
+  const { getters } = useContext(VideosContext);
+  const { bookmarkedMovies, filteredBookmarkedVideos, bookmarkedSeries } =
+    getters;
+  return (
+    <>
+      <Searchbar placeholder="Search for bookmarked shows" />
+      <div className="bookmarked-videos">
+        {filteredBookmarkedVideos().length === 0 && (
+          <h2 className="no-bookmark">You currenly have no bookmark.</h2>
+        )}
+
+        {bookmarkedMovies().length > 0 && (
+          <VideoList
+            className="bookmarked-movies"
+            title="Bookmarked Movies"
+            videos={bookmarkedMovies()}
+          />
+        )}
+        {bookmarkedSeries().length > 0 ? (
+          <VideoList
+            className="bookmarked-tv-series"
+            title="Bookmarked TV Series"
+            videos={bookmarkedSeries()}
+          />
+        ) : (
+          <VideoList
+            className="results"
+            title="`Found ${filteredBookmarkedVideos().length} results for '${search}'`"
+            videos={filteredBookmarkedVideos()}
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Bookmark;
