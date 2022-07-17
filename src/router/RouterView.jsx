f0e8b77eab7e1451245abb276e4922d9e80403f5
Soufianetyk1337 <sz.fanel@gmail.com>
2022-07-18 00:30:03 +0100

    Adding React Lazy Loading to React Router

--- src/router/RouterView.jsx
+++ src/router/RouterView.jsx
@@ -1,22 +1,43 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/components/Home/Home";
-import Bookmark from "@/components/Bookmark/Bookmark";
-import Series from "@/components/Series/Series";
-import Movies from "@/components/Movies/Movies";
 import React from "react";
 import Navbar from "@/components/Navbar/Navbar";
 
 const RouterView = () => {
+  const Bookmark = React.lazy(() => import("@/components/Bookmark/Bookmark"));
+  const Series = React.lazy(() => import("@/components/Series/Series"));
+  const Movies = React.lazy(() => import("@/components/Movies/Movies"));
   return (
     <BrowserRouter>
       <Navbar />
       <main className="main">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/series" element={<Series />} />
-          <Route path="/bookmarks" element={<Bookmark />} />
-          <Route path="/:notfound(.*)/*" element={<h1>Not Found</h1>} />
+          <Route path="/" index element={<Home />} />
+          <Route
+            path="/movies"
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <Movies />
+              </React.Suspense>
+            }
+          />
+          <Route
+            path="/series"
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <Series />
+              </React.Suspense>
+            }
+          />
+          <Route
+            path="/bookmarks"
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <Bookmark />
+              </React.Suspense>
+            }
+          />
+          <Route path="*" element={<h1>Route Not Found</h1>} />
         </Routes>
       </main>
     </BrowserRouter>
