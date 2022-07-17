91693bd925969473f9479c1401530d3d4dd520af
Soufianetyk1337 <sz.fanel@gmail.com>
2022-07-17 23:28:50 +0100

    Adding Swipe Carossel to the trending Section

--- src/components/Trending/TrendingSection/TrendingSection.jsx
+++ src/components/Trending/TrendingSection/TrendingSection.jsx
@@ -1,19 +1,62 @@
 import React from "react";
 import TrendingVideo from "../TrendingVideo/TrendingVideo";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import useBreakpoint from "@/hooks/useBreakpoints";
+// Import Swiper styles
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+import "swiper/scss/scrollbar";
+import "swiper/css/effect-fade";
 import "./TrendingSection.scss";
-const TrendingSection = ({ videos = [] }) => {
+const slidesCount = (width) => {
+  let count = 0;
+  switch (width) {
+    case "xs":
+    case "sm":
+      count = 1;
+      break;
+    case "md":
+      count = 2;
+      break;
+    case "lg":
+      count = 3;
+      break;
+    default:
+      break;
+  }
+  return count;
+};
+const TrendingSection = ({ videos }) => {
+  const breakpoint = useBreakpoint();
   return (
     <section className="trending">
       <h2 className="trending__title">Trending</h2>
       <ul className="trending__videos">
-        {videos.length &&
-          videos.map((video) => {
-            return (
-              <li key={video.title} className="trending__video">
-                <TrendingVideo video={video} />
-              </li>
-            );
-          })}
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={50}
+          // slidesPerView={3}
+          slidesPerView={slidesCount(breakpoint)}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+          effect="fade"
+        >
+          {videos.length &&
+            videos.map((video) => {
+              return (
+                <SwiperSlide key={video.title}>
+                  <li key={video.title} className="trending__video">
+                    <TrendingVideo video={video} />
+                  </li>
+                </SwiperSlide>
+              );
+            })}
+        </Swiper>
       </ul>
     </section>
   );
